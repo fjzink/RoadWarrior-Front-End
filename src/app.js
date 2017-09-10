63004f4dcfba083bd692a2cf971ff7665bc1e2ad
Fulton Zink <joeyzink@gmail.com>
2017-09-09 21:31:50 -0700

    Add common components to the common folder. Add a LoginForm component.

--- src/app.js
+++ src/app.js
@@ -1,13 +1,14 @@
 import React, { Component } from 'react';
 import { View, Text } from 'react-native';
-import Header from './components/common/Header';
+import { Header } from './components/common/';
+import LoginForm from './components/LoginForm';
 
 class App extends Component {
   render() {
     return (
       <View>
         <Header headerText="Authentication" />
-        <Text>An app!</Text>
+        <LoginForm />
       </View>
     );
   }
