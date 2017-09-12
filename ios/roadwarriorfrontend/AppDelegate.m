aa0d23b2f87f29fc28a197dce53ed78c1033b912
peanutenthusiast <mhdedios@gmail.com>
2017-09-11 22:14:14 -0700

    Import react-native-maps into front-end, link react-native-maps with react-native app

--- ios/roadwarriorfrontend/AppDelegate.m
+++ ios/roadwarriorfrontend/AppDelegate.m
@@ -11,12 +11,15 @@
 
 #import <React/RCTBundleURLProvider.h>
 #import <React/RCTRootView.h>
+@import GoogleMaps;
 
 @implementation AppDelegate
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
   NSURL *jsCodeLocation;
+  
+  [GMSServices provideAPIKey:@"AIzaSyDv46VRrktCpx1fCm3piqCSsMRajVCd6rk"];
 
   jsCodeLocation = [[RCTBundleURLProvider sharedSettings] jsBundleURLForBundleRoot:@"index.ios" fallbackResource:nil];
 
--- src/app.js
+++ src/app.js
@@ -2,11 +2,12 @@ import { StackNavigator } from 'react-navigation';
 import LoginForm from './components/LoginForm';
 import RegistrationForm from './components/RegistrationForm';
 import MapScreen from './components/MapScreen';
+import Map from './components/Map';
 import React from 'react';
 
 const App = StackNavigator({
   Login: { screen: LoginForm },
-  Map: { screen: MapScreen },
+  Map: { screen: Map },
   Register: { screen: RegistrationForm }
 });
 
